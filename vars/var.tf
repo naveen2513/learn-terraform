5bdd2b7bb1e6dd026b959d5893a0c6606ad3e58e
naveen p <naveenpendekatla0407@gmail.com>
2023-05-06 16:31:51 +0530

    terraform

---
+++ vars/var.tf
@@ -0,0 +1,9 @@
+variable "sample_string" {
+  default = "hello world"
+}
+output "sample_string" {
+  value = var.sample_string
+}
+output "sample_string1" {
+  value = "value of sample_string=${var.sample_string}"
+}
