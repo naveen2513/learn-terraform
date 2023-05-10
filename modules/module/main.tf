6fc2b792537cb9d34d71b619d1e781a0841d0888
naveen p <naveenpendekatla0407@gmail.com>
2023-05-10 08:32:21 +0530

    terraform

--- modules/module/main.tf
+++ modules/module/main.tf
@@ -1,15 +1,15 @@
 resource "null_resource" "nothing" {
   provisioner "local-exec" {
-    command = "hello from naveen"
+    command = "echo hello from module"
   }
 }
 resource "null_resource" "nothing1" {
   provisioner "local-exec" {
-    command = "hello from naveen"
+    command = "echo hello from module"
   }
 }
 resource "null_resource" "nothing2" {
   provisioner "local-exec" {
-    command = "hello from naveen"
+    command = "echo hello from module"
   }
 }
