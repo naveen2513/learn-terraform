241dbfa7ce27d8c71370b40afcc682d26d59be08
naveen p <naveenpendekatla0407@gmail.com>
2023-05-22 16:12:56 +0530

    servers

--- data-sources/main.tf
+++ data-sources/main.tf
@@ -5,3 +5,8 @@ data "aws_security_group" "selected" {
 output "aws_security_group" {
   value = data.aws_security_group.selected
 }
+
+data "aws_security_groups" "test" {}
+output "all_sq" {
+  value = data.aws_security_groups.test
+}
